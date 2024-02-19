db10bb72002f3d5fae18c2ec25a89c278d2390b7
Maria Luiza Rodrigues <marialuiza.r.eng@gmail.com>
2024-02-19 10:17:21 -0300

    feat: adding slide style

--- src/app.tsx
+++ src/app.tsx
@@ -1,11 +1,13 @@
-import FullBanner from "./components/full-banner";
 import Header from "./components/header";
+import FullBanner from "./components/full-banner";
+import Offers from "./components/offers";
 
 function App() {
   return (
     <>
       <Header />
       <FullBanner />
+      <Offers />
     </>
   );
 }
---
+++ src/components/offers.tsx
@@ -0,0 +1,3 @@
+export default function Offers() {
+  return <h1>Hello</h1>;
+}
