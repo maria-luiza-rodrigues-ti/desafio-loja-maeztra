67bf83efd5b6430505d584411ea1c2fa163e4621
Maria Luiza Rodrigues <marialuiza.r.eng@gmail.com>
2024-02-19 11:23:19 -0300

    feat: adding slider to offers section

--- src/components/offers.tsx
+++ src/components/offers.tsx
@@ -1,3 +1,40 @@
+import items from "../data/offers.json";
+
+// Import Swiper React components
+import { Swiper, SwiperSlide } from "swiper/react";
+
+// Import Swiper styles
+import "swiper/css";
+
 export default function Offers() {
-  return <h1>Hello</h1>;
+  const { offers } = items;
+
+  return (
+    <section className="flex flex-col items-center mt-6 mb-[74px]">
+      <h4 className="text-base text-lightBlack font-bold">
+        Por que comprar na Maeztra?
+      </h4>
+      <Swiper
+        slidesPerView={5}
+        spaceBetween={15}
+        loop={true}
+        className="flex justify-between max-w-[1650px] w-full gap-[15px] mt-[21px]"
+      >
+        {offers.map((offer, index) => (
+          <SwiperSlide
+            className="flex bg-grey pt-[11px] px-[27px] pb-[14px] gap-x-[22px] rounded"
+            key={index}
+          >
+            <img src={offer.icon} alt="" />
+            <div className="flex flex-col justify-center">
+              <h5 className="text-lightBlack font-bold text-sm">
+                {offer.title}
+              </h5>
+              <p className="text-black text-xs">{offer.description}</p>
+            </div>
+          </SwiperSlide>
+        ))}
+      </Swiper>
+    </section>
+  );
 }
