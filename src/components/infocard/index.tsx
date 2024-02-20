6e5ed94b8750d53ba9f1da117ecb157a59715b3d
Maria Luiza Rodrigues <marialuiza.r.eng@gmail.com>
2024-02-20 14:17:23 -0300

    chore: refact infocard and adding mobile style

--- src/components/infocard/index.tsx
+++ src/components/infocard/index.tsx
@@ -1,9 +1,13 @@
+import useMobile from "../../hooks/useMobile";
+
 export default function Infocard() {
+  const mobile = useMobile();
+
   return (
-    <section className="flex justify-center items-center gap-16 max-w-[1600px] w-full mx-auto my-[80px]">
+    <section className="flex justify-center items-center flex-wrap md:gap-16 max-w-[1600px] w-full px-[30px] md:px-0 pt-[23px] pb-[39px] md:mx-auto md:my-[80px]">
       <div>
         <h3 className="text-black text-2xl font-bold mb-4">Lorem ipsum </h3>
-        <p className="text-black text-sm">
+        <p className="text-black text-sm mb-6 md:mb-0">
           Lorem ipsum dolor sit amet, consectetur adipiscing elit. Interdum
           mauris posuere fusce proin mattis. Hendrerit cras ut nunc enim dictum.
           Mattis proin ut quis donec sed eget nulla. Vel mi ut mauris integer.
@@ -15,7 +19,15 @@ export default function Infocard() {
           sit sapien.
         </p>
       </div>
-      <img src="./src/assets/banner-2.png" alt="Banner" />
+      {!mobile ? (
+        <img src="./src/assets/banner-2.png" alt="Banner" className="rounded" />
+      ) : (
+        <img
+          src="./src/assets/banner-2-mobile.png"
+          alt="Banner"
+          className="rounded"
+        />
+      )}
     </section>
   );
 }
