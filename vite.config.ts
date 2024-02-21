e9dea776077831539562bc6f03a9eec9e439873b
Maria Luiza Rodrigues <marialuiza.r.eng@gmail.com>
2024-02-21 15:05:53 -0300

    feat: adding gh-pages configs

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/desafio-loja-maeztra",
+  base: "/desafio-loja-maeztra/",
 });
